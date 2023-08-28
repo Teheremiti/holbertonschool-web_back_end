51c9a8d2a277cb684401da66d603d979e3e0c7a6
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 12:08:41 +0200

    Try type aliasing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Type-annotated function sum-list with complex types"""
 
+vector = list[float]
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: vector) -> float:
     """Returns the sum of the floats in input_list"""
     sum = 0
     for num in input_list:
