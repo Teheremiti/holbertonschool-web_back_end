abfa8d270e13a23524957230f0690749d7609d57
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 18:50:05 +0200

    Complex types - functions

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type-annotated function make_multiplier"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+    
+    def mul(n: float) -> float:
+        """Inner function that returns the product of n and multipler"""
+        return n * multiplier
+    
+    return mul
