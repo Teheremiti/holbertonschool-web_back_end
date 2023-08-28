16b57959f1fdf60bd82649e58b3cf2c5a22d29de
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 19:01:36 +0200

    pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,9 +6,9 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by multiplier"""
-    
+
     def mul(n: float) -> float:
         """Inner function that returns the product of n and multipler"""
         return n * multiplier
-    
+
     return mul
