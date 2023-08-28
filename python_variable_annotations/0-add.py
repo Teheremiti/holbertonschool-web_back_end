a9c95224a8170aa39413e67636e8d7664bfd463a
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 11:10:35 +0200

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Type-annotated function that adds two floats"""
 
+
 def add(a: float, b: float) -> float:
     """Returns the sum of the floats a and b"""
     return a + b
