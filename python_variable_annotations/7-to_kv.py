2030fbba12969199269f1de3ea9491aa4db44214
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 12:20:25 +0200

    Complex types - tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv"""
+
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple that consists of k and v squared"""
+    return (k, v**2)
