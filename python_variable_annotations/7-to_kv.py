dbe20d2585edfe93a9e12bf87f6f16225b7916c0
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-28 12:21:03 +0200

    pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple that consists of k and v squared"""
     return (k, v**2)
