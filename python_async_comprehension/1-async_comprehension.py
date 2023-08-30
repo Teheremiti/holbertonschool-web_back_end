c445ec09b0e26f1e2138d58f0eb610656dbaca89
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-08-30 06:30:03 +0200

    Async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Coroutine async_comprehension that collects 10 numbers using
+async comprehensing over async generator"""
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers using an async comprehensing over
+    async_generator, then returns the 10 random numbers"""
+    numbers = []
+    async for i in async_generator():
+        numbers.append(i)
+    return numbers
