157a845cec248e88e4f4452f546538992f315c0a
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-04 21:51:29 +0200

    Add status checks

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,13 +8,16 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_co = client.logs.nginx
     
-    print("{} logs\n\
-          Methods:".format(nginx_co.count()))
+    print("{} logs\nMethods:".format(nginx_co.count_documents({})))
     
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     logs_dict = {}
     for method in methods:
-        logs_dict[method] = nginx_co.count({ "method": method })
+        logs_dict[method] = nginx_co.count_documents({ "method": method })
 
     for method, logs in logs_dict.items():
         print("\tmethod {}: {}".format(method, logs))
+
+    status_checks = nginx_co.count_documents({ "method": "GET",
+                                               "path": "/status"})
+    print("{} status check".format(status_checks))
