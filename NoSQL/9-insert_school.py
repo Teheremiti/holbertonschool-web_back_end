b2525322655dc301b1579beefe2d00848007b1f3
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-04 15:32:35 +0200

    Fix: Return value

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,4 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """Returns the _id of the new document"""
-    return mongo_collection.insert_one(kwargs)
+    new = mongo_collection.insert_one(kwargs)
+    return new.inserted_id
