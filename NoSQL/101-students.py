6e2396448c4248aa8e425ceed0bbef06b894e1a3
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-05 19:14:02 +0200

    Fix: Return

--- NoSQL/101-students.py
+++ NoSQL/101-students.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""Method top_students that lists all students in a dict
-sorted by average score"""
+"""Method top_students that lists all students sorted by average score"""
 
 
 def top_students(mongo_collection):
     """Returns all students sorted by average score"""
-    avg_scores = {}
     for student in mongo_collection.find():
         total = 0
         number_of_topics = 0
         for topic in student['topics']:
             total += topic['score']
             number_of_topics += 1
-        avg_scores[student['name']] = total / number_of_topics
+        student['averageScore'] = total / number_of_topics
     
-    return dict(sorted(avg_scores.items(), key = lambda item: item[1],
-                       reverse=True))
+    return sorted(mongo_collection, key = lambda item: item[2], reverse=True)
