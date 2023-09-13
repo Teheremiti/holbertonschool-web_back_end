8fabc613b85b1a182ef30005eb46c0a88beda62a
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-13 17:00:59 +0200

    Disable eslint

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,9 @@
+/* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
     const budget = {
         income,
         gdp,
-        capita,
+        capita
     };
 
     return budget;
