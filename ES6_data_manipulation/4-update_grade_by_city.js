fcf57558c6bc6f8f4d5497a6aa8abccae0a0014d
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-04 17:00:02 +0200

    Update student grade by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  return studentsList.filter((student) => student.location === city)
+    .map((student) => {
+      for (const newGrade of newGrades) {
+        let finalGrade = 'N/A'
+        if (student.id === newGrade.studentId) {
+          finalGrade = newGrade.grade;
+        }
+        student.grade = finalGrade;
+      }
+    });
+}
