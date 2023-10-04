7d394827de92c9c64fe588c82bc00d2703cc9102
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-04 17:08:54 +0200

    Create an Int8 typed array

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  return new DataView(buffer).setInt8(position, value);
+}
