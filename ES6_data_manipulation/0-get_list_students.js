54ef9bc0a7b922d281c1126b52d16ab3f866b57e
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-04 16:22:30 +0200

    Get students list

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const firstStudent = {id: 1, firstName: 'Guillaume', location: 'San Francisco'};
+  const secondStudent = {id: 2, firstName: 'James', location: 'Columbia'};
+  const thirdStudent = {id: 5, firstName: 'Serena', location: 'San Francisco'};
+  return [firstStudent, secondStudent, thirdStudent];
+}
