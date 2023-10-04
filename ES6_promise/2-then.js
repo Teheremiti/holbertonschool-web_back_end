0c901610f8ddab639f5d279c0470c2fe81302c2f
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-04 13:43:21 +0200

    eslint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => ({
-      status: 200,
-      body: 'success',
+    status: 200,
+    body: 'success',
   })).then((response) => {
     console.log('Got a response from the API');
     return response
