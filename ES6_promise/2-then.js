abd9fb87d79cb064ff9b08baa5a50c62b9e5c99c
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-04 13:42:55 +0200

    Handle response from API

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({
+      status: 200,
+      body: 'success',
+  })).then((response) => {
+    console.log('Got a response from the API');
+    return response
+  }).catch(() => new Error());   
+}
