d5ff8bf9f3e73894505a3a409ed515257138a9fc
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-05 12:46:16 +0200

    Handle error cases

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
+  if (!startString || !(startString instanceof String)) {
+    return '';
+  }
+
   const string = '';
   const sep = '';
   for (const ele of set.values()) {
-    if (ele.startsWith(startString)) {
+    if (ele && ele.startsWith(startString)) {
       string += sep + ele.slice(startString.length);
       sep = '-';
     }
