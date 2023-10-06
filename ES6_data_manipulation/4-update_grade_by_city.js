bd0b9d09b5408e1a824b64af1e4f73b0186e3ab4
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-06 10:30:23 +0200

    Return the result :)

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,13 +2,14 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
   return studentsList.filter((student) => student.location === city)
     .map((student) => {
+      const studentUpdate = { ...student };
       for (const newGrade of newGrades) {
         let finalGrade = 'N/A'
         if (student.id === newGrade.studentId) {
           finalGrade = newGrade.grade;
         }
-        const studentUpdate = { ...student };
         studentUpdate.grade = finalGrade;
       }
+      return studentUpdate;
     });
 }
