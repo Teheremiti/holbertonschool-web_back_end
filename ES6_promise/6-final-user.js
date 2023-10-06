8b5857362e7be6a7181cd642133bb6e8132f4b72
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-06 14:17:09 +0200

    Use .allSettled instead of .all

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
+/* eslint-disable */
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
       const promises = [];
       results.forEach((promise) => {
         promises.push({
           status: promise.status,
-          value: promise.value,
+          value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
         });
       });
       return promises;
